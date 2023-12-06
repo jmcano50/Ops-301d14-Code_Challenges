6ff08d6ea7a4e839f5f7992bef55d3b34e124fbb
jmcano50 <jcano@aol.com>
2023-12-06 01:00:00 -0800

    301Ops7.py

--- 301Ops7.py
+++ 301Ops7.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
+# Script Name:                  Ops Challenge: Directiory Creation
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      12/05/2023
+# Purpose:                      Traverse through directories, subdirectories and files in the provided path 
+# Execution:                    31Ops7.py
+# Resource:                     
+
+
 
 # Import libraries
 
 import os
 
-# Declaration of variables
+def generate_directory_structure():
+    # Ask the user for a directory path
+    user_input_path = input("Enter a directory path: ")
 
-### Read user input here into a variable
+    # Check if the provided path exists
+    if not os.path.exists(user_input_path):
+        print("Error: The specified path does not exist.")
+        return
 
-# Declaration of functions
+    #Use os.walk() to get all directories, sub-directores, and files
+    for root, dirs, files in os.walk(user_input_path):
+        print(f"Current directory: {root}")
+        print(f"Subdirectories: {dirs}")
+        print(f"Files: {files}")
 
-### Declare a function here
+# Call the function to generate and display the directory structure
+generate_directory_structure()
 
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
-
-# Main
-
-### Pass the variable into the function here
-
-# End
