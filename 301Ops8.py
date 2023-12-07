28a4bfbe59b985274585ce8fbe11d6b10388ec87
jmcano50 <jcano@aol.com>
2023-12-06 19:42:52 -0800

    301Ops8.py

---
+++ 301Ops8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Script Name:                  Ops Challenge: 
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      12/06/2023
+# Purpose:                      
+# Execution:                    31Ops8.py
+# Resource:                     
+
+# Import libraries
