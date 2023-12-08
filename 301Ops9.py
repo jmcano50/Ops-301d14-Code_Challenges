a1517d92792b41f6e3cc268cdfe457147058026b
jmcano50 <jcano@aol.com>
2023-12-07 21:41:03 -0800

    301Ops9.py

---
+++ 301Ops9.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Script Name:                  Ops Challenge: Python Conditional Statements
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      12/07/2023
+# Purpose:                      
+# Execution:                    31Ops9.py
+# Resource:                     
+
+# Import libraries
