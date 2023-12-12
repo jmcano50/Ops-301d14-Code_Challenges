c7e71c86c6f6d19fc2c1f12f0d4bf3a751bc6ea6
jmcano50 <jcano@aol.com>
2023-12-11 22:07:54 -0800

    301Ops11.py

--- 301Ops11.py
+++ 301Ops11.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
-# Script Name:                  Ops Challenge: Python File handling
+# Script Name:                  Ops Challenge: Install Psutil
 # Author:                       Juan Miguel Cano
-# Date of latest revision:      12/08/2023
-# Purpose:                      This code displays Python's file handling capabilities.                   
+# Date of latest revision:      12/12/2023
+# Purpose:                      Create a Python script that fetches this information using Psutil
 # Execution:                    31Ops10.py
-# Resource:                     
-
-
 
 import psutil
-cpu_time = str (psutil.cpu_times())
-print('CPU Time1: ' + cpu_time+ ".")
 
-print(f'CPU Time2: {cpu_time}.')
+def fetch_cpu_times():
+    # Get CPU times
+    cpu_times = psutil.cpu_times()
 
-print(psutil.cpu_times)
+    # Extracting the required information
+    return {
+        'Time spent by normal processes in user mode': cpu_times.user,
+        'Time spent processes in kernel mode': cpu_times.system,
+        'Time when system was idle': cpu_times.idle,
+        'Time spent by priority processes in user mode': cpu_times.nice,
+        'Time spent for servicing hardware interrupts': cpu_times.iowait,
+        'Time spent for servicing software interrupts': cpu_times.irq,
+        'Time spent by other operating systems in a virtualized environment': cpu_times.steal,
+        'Time spent running a virtual CPU for guest OS under the control of the Linux kernel': cpu_times.guest
+    }
+
+# Call the function and print the results
+try:
+    cpu_times_info = fetch_cpu_times()
+    for key, value in cpu_times_info.items():
+        print(f"{key}: {value} seconds")
+except Exception as e:
+    print(f"An error occurred: {e}")
