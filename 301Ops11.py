d35fbd7a97355db36fc0aae9e43bd53da88389b0
jmcano50 <jcano@aol.com>
2023-12-11 21:33:13 -0800

    301Ops11.py

--- 301Ops11.py
+++ 301Ops11.py
@@ -1,4 +1,10 @@
-
+#!/usr/bin/env python3
+# Script Name:                  Ops Challenge: Python File handling
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      12/08/2023
+# Purpose:                      This code displays Python's file handling capabilities.                   
+# Execution:                    31Ops10.py
+# Resource:                     
 
 
 
