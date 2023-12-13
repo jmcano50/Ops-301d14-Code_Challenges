cca13188cc904ae3e69b51da4d5b75a9fed2bd0a
jmcano50 <jcano@aol.com>
2023-12-12 21:20:38 -0800

    301Ops12.py

---
+++ 301Ops12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Script Name:                  Ops Challenge: Python Requests Library
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      12/13/2023
+# Purpose:                      Security professional use to evaluate how a web server responds to outside requests.
+# Execution:                    31Ops10.py
+# Resources:     
+
+
+
+import requests
+
