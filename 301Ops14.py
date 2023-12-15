d6364c02e589876f46cdcd4b4612cd0b6051e9bc
jmcano50 <jcano@aol.com>
2023-12-14 20:10:00 -0800

    301Ops14.py

---
+++ 301Ops14.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python3
+# Script Name:                  Ops Challenge: Python Malware Analysis
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      12/14/2023
+# Purpose:                      
+# Execution:                    31Ops14.py
+# Resources:                    
+
+
+
+
+
+
+
+
